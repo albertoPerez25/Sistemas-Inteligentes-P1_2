338806b845cf02d14d968f6e5d0cdde0f78d7697
alberto <alberto.perez25@alu.uclm.es>
2024-10-28 17:37:29 +0100

    cambiadas rutas

--- src/main.py
+++ src/main.py
@@ -2,17 +2,16 @@ from clasesBasicas import Problema
 from BusquedasNoInformadas import Anchura, Profundidad
 from BusquedasInformadas import PrimeroMejor,AEstrella
 
-Al_Huge2 = '/home/calberto/Documents/Uni/5toCuatri/SistemasInteligentes/Practicas/P1/Sistemas-Inteligentes-P1_2/problems/huge/calle_herreros_albacete_2000_2.json'
-Al_Large1 = '/home/calberto/Documents/Uni/5toCuatri/SistemasInteligentes/Practicas/P1/Sistemas-Inteligentes-P1_2/problems/large/calle_agustina_aroca_albacete_1000_2.json'
-Al_Huge1 = '/home/calberto/Documents/Uni/5toCuatri/SistemasInteligentes/Practicas/P1/Sistemas-Inteligentes-P1_2/problems/huge/calle_del_virrey_morcillo_albacete_2000_4.json'
-Al_Test1 = '/home/calberto/Documents/Uni/5toCuatri/SistemasInteligentes/Practicas/P1/Sistemas-Inteligentes-P1_2/problems/test/test.json'
-Al_Test2 = '/home/calberto/Documents/Uni/5toCuatri/SistemasInteligentes/Practicas/P1/Sistemas-Inteligentes-P1_2/problems/test/test2.json'
-Al_Medium1 = '/home/calberto/Documents/Uni/5toCuatri/SistemasInteligentes/Practicas/P1/Sistemas-Inteligentes-P1_2/problems/medium/calle_del_virrey_morcillo_albacete_2000_4.json'
-Al_Small1 = '/home/calberto/Documents/Uni/5toCuatri/SistemasInteligentes/Practicas/P1/Sistemas-Inteligentes-P1_2/problems/small/calle_del_virrey_morcillo_albacete_250_3.json'
-Ma_Medium1 = '/home/marcos/Documentos/3_Uni/Sistemas_Inteligentes/Programas_python/SInteligentesP1/recursos/problems/medium/calle_del_virrey_morcillo_albacete_2000_4.json'
-Ma_Test1 = '/home/marcos/Documentos/3_Uni/Sistemas_Inteligentes/Programas_python/SInteligentesP1/recursos/problems/test/test.json'
+Huge2 = 'problems/huge/calle_herreros_albacete_2000_2.json'
+Large1 = 'problems/large/calle_agustina_aroca_albacete_1000_2.json'
+Large2 = 'problems/large/calle_f_albacete_5000_4.json'
+Huge1 = 'problems/huge/calle_del_virrey_morcillo_albacete_2000_4.json'
+Test1 = 'problems/test/test.json'
+Test2 = 'problems/test/test2.json'
+Medium1 = 'problems/medium/calle_del_virrey_morcillo_albacete_2000_4.json'
+Small1 = 'problems/small/calle_del_virrey_morcillo_albacete_250_3.json'
 
-RUTAJSON = Al_Large1
+RUTAJSON = Large2
 
 anchura = Anchura(Problema(RUTAJSON))
 profundidad = Profundidad(Problema(RUTAJSON))
