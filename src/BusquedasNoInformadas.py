d68cf4816a209c8c664ef4adf828da4f9ee169b1
Marcos <Marcos.Lopez14@alu.uclm.es>
2024-10-31 20:39:05 +0100

    comit

--- src/BusquedasNoInformadas.py
+++ src/BusquedasNoInformadas.py
@@ -5,20 +5,14 @@ from claseBusqueda import Busqueda
 class BusquedaNoInformada(Busqueda,metaclass=ABCMeta):
     def __init__(self, problema):
         super().__init__(problema)
-        self.frontera = []  # Lista de nodos a ser expandidos
+        # Frontera se usará como lista de nodos a ser expandidos
 
     def añadirNodoAFrontera(self, nodo, frontera):  # Es igual en Anchura y Profundidad
-        if isinstance(nodo, list):                  # Añadimos los nodos nuevos a frontera ordenados por ID
-            self.frontera.extend(sorted(nodo))      # Así nos ahorramos ordenar sucesores por ID en busquedas informadas
-        else:
-            frontera.append(nodo)
+        frontera.append(nodo)
 
     @abstractmethod
     def extraerNodoDeFrontera(self, frontera):
         pass
-
-    def esVacia(self, frontera):                    # Es igual en Anchura y Profundidad
-        return len(frontera) != 0
 
 class Anchura(BusquedaNoInformada):
     def extraerNodoDeFrontera(self, frontera):
--- src/claseBusqueda.py
+++ src/claseBusqueda.py
@@ -1,27 +1,33 @@
 #clase abstracta que tendrá busqueda(), expandir(), estadisticas() e imprimirResultado()
-from clasesBasicas import Nodo,Problema
-from abc import ABC, ABCMeta,abstractmethod
-import time,heapq
+from clasesBasicas import Nodo
+from abc import ABC,abstractmethod
+import time
+from heapq import heappop
 
 class Busqueda(ABC):
     def __init__(self, problema):
+        self.frontera = []  # Declaración igual en todos los algoritmos
+                            # heapq usa una lista como priority vacia.
+                            # heapq es algo mas rapido que PriorityQueue al 
+                            # no prevenir errores de hilos. Como no usamos concurrencia 
+                            # de hilos en esta práctica, heapq es ideal
+                            # https://docs.python.org/3/library/heapq.html
+                            # https://docs.python.org/3/library/queue.html#queue.PriorityQueue
         self.problema = problema
-        self.estadoInicial = problema.Inicial
-        self.estadoFinal = problema.Final
         self.tInicio = 0
         self.tFinal = 0
-        self.cerrados = set()        # para no volver a expandir nodos ya visitados
+        self.cerrados = set()        # Para no volver a expandir nodos ya visitados
         self.nodo = Nodo(problema.Inicial)
-        self.frontera = None
-        #estadisticas:
+        # Estadisticas:
         self.nExpandidos = 0
         self.nProfundidad = 0
         self.nCosteTotal = 0           
         self.nGenerados = 0
 
     def expandir(self,nodo,problema):
-        sucesores = []
-        for accion in problema.getAccionesDe(nodo.estado.identifier):
+        acciones = problema.getAccionesDe(nodo.estado.identifier)
+        while acciones:
+            accion = heappop(acciones)
             sucesor = Nodo(problema.getEstado(accion.destination))
             sucesor.padre = nodo
             sucesor.accion = accion
@@ -29,39 +35,44 @@ class Busqueda(ABC):
             sucesor.profundidad = nodo.profundidad + 1
             self.nGenerados = self.nGenerados + 1
             sucesor.nGenerado = self.nGenerados
-            sucesores.append(sucesor)
-        return sucesores
-    
+            self.añadirNodoAFrontera(sucesor, self.frontera)    # Añadimos los sucesores a frontera.
+                                                                # Nos ahorramos un bucle For al añadirlos 
+                                                                # desde expandir
+                                                            
     def busqueda(self):
         self.tInicio = time.time()
         self.añadirNodoAFrontera(self.nodo,self.frontera)
-        while(self.esVacia(self.frontera)):
+        while(not self.esVacia(self.frontera)):
             self.nodo = self.extraerNodoDeFrontera(self.frontera)
             if (self.testObjetivo(self.nodo)):
                 self.tFinal = time.time()
                 return self.listaAcciones(self.nodo)
             if (not self.nodo.estado.identifier in self.cerrados):
-                sucesores = self.expandir(self.nodo, self.problema)     # Obtenemos los sucesores con Expandir()
-                self.añadirNodoAFrontera(sucesores, self.frontera)      # Añadimos los sucesores a frontera.
+                self.expandir(self.nodo, self.problema)     # Obtenemos los sucesores con Expandir()
                 self.nExpandidos = self.nExpandidos + 1
                 self.cerrados.add(self.nodo.estado.identifier)
-        raise Exception("Frontera vacia")
+        self.tFinal = time.time()
+        return self.imprimirResultado([])
 
     def testObjetivo(self,nodo):
         return nodo.estado.__eq__(self.problema.Final)
 
     def listaAcciones(self,nodo):
         sol = []                         # Lista de acciones que han llevado desde el final al inicial
+        estados = []                     # Lista de ids de los estados desde el final al inicial
         self.nCosteTotal = nodo.coste    # nodo.coste es acumulativo
         self.nProfundidad=nodo.profundidad
         while (nodo.padre):
             sol.append(nodo.accion)
+            estados.append(nodo.estado.identifier)
             nodo = nodo.padre
+        estados.append(nodo.estado.identifier) # Añadimos el inicial
         sol.reverse()                   # Ahora es una lista de acciones desde el inicial al final
         self.imprimirResultado(sol)
-        return sol
+        return estados
 
     def imprimirResultado(self,sol):
+        if not sol : print("Solución no encontrada")
         print("Nodos generados:",self.nGenerados)
         print("Nodos expandidos:",self.nExpandidos)
         print("Tiempo de ejecución:",(self.formatearTiempo(self.tFinal - self.tInicio)),"segundos")
@@ -69,12 +80,15 @@ class Busqueda(ABC):
         print("Coste de la solución:",self.formatearTiempo(self.nCosteTotal))
         print("Solución:",sol)
     
-    def formatearTiempo(self, tiempo):
+    def formatearTiempo(self, tiempo):  # Para imprimir los tiempos como en las soluciones
         horas = int(tiempo // 3600)
         minutos = int((tiempo % 3600) // 60)
         segundos = int(tiempo % 60)
         milisegundos = int((tiempo - int(tiempo)) * 1000000)
         return f"{horas:01d}:{minutos:02d}:{segundos:02d}.{milisegundos:06d}"
+
+    def esVacia(self, frontera): # Igual en todos los algoritmos
+        return len(frontera) == 0
 
     @abstractmethod
     def añadirNodoAFrontera(self, nodo, frontera):
@@ -82,7 +96,3 @@ class Busqueda(ABC):
     @abstractmethod
     def extraerNodoDeFrontera(self, frontera):
         pass
-    @abstractmethod
-    def esVacia(self, frontera):
-        pass
-
